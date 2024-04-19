8198db1d55163e5a965bbc0a54c284e8a35f22a7
MartinezJauregui <martinezjaureguijavier@cbtis116.edu.mx>
2024-04-19 15:16:57 -0700

    code

--- pictures/src/API.js
+++ pictures/src/API.js
@@ -1,14 +1,17 @@
-import axios from "axios";
+import axios from 'axios'
 
 const searchImage = async(term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client_id=qvYwjG3WM0I88-zQCmfFGP3XCMuoY6p5zuGk-h9qOvs'
-    
-    const response =  await axios.get(url,{
+    const url = 'https://api.unsplash.com/search/photos/?client_id=HIbs4qIH8mS6EI2l88CK7llO2apuCUSq88zx9zLTJ48'
+    const response = await axios.get(url, {
         params: {
-            query: 'cars'
+            query: term
         }
     })
-    console.log(response)
+
+    console.log(term)
+    console.log(response.data.results)
+
+    return response.data.results
 }
 
 export default searchImage
--- pictures/src/App.jsx
+++ pictures/src/App.jsx
@@ -1,18 +1,28 @@
 import { useState } from 'react'
-import ImageList from "./components/Imagelist.jsx "
-import ImageShow from "./components/ImageShow.jsx"
-import SearchBear from "./components/SearchBear.jsx"
+import SearchBar from './components/SearchBar'
+import ImageList from './components/ImageList'
+import searchImage from './API'
 import './App.css'
-const [images, setImages] = useState('')
-const handleSubimit =async(term)=> {
-  console.log('usted esta buscado:' , term)
 
-}
 function App() {
+  const [images, setImages] = useState([])  
+
+  const handleSubmit = async(term) => {
+    console.log('uste esta buscando con:', term)
+    const result = await searchImage(term)
+
+    console.log('coco')
+    console.log(result)
+
+    setImages(result)
+
+  }
+
   return (
     <div>
       <h1>App</h1>
-      <SearchBear onSubmit ={handleSubimit}/>
+      <SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images} />
     </div>
   )
 }
